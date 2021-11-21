7315fba64b7eb1f3e3e99848c9fa4826328e2911
Máté Tóth <e11915294@stundent.tuwien.ac.at>
2021-11-21 15:00:44 +0100

    UI for the login created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,6 +12,13 @@ import {LoginComponent} from './components/login/login.component';
 import {MessageComponent} from './components/message/message.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {httpInterceptorProviders} from './interceptors';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -26,9 +33,16 @@ import {httpInterceptorProviders} from './interceptors';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    MatInputModule,
     HttpClientModule,
     NgbModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule
   ],
   providers: [httpInterceptorProviders],
   bootstrap: [AppComponent]
